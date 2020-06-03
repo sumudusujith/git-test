52b461ace1ca49eab6959a60209ba7a5b205fd7e
sumudusujith <sumudu.2019582@iit.ac.lk>
2020-06-03 15:08:05 +0530

    Second Change

--- Readme.txt
+++ Readme.txt
@@ -1,1 +1,3 @@
-rewre
+fsdfsdf
+fdsfek
+vcdefe
--- new.py
+++ new.py
@@ -1,1 +1,24 @@
-sdswd
+##Name : Sumudu Herath
+#Date: 26th Feb 2020
+#Description : Tutorial 03
+
+#Question 01
+#part a
+
+n = int(input("Give me a number over 100:"))
+try:
+    x=int(n)
+    if x<=100:
+        print(n,'is not over 100')
+
+except ValueError:
+    print("Please enter an valid integer")
+
+#part b
+n=input("Please enter your age")
+try:
+    x=int(n)
+    if n>=18:
+        print("Five can vote")
+except ValueError:
+    print("Please enter an valid integer,")
